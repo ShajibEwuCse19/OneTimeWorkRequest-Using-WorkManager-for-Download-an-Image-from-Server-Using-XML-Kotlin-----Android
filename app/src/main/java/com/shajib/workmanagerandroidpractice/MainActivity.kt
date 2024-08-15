c8d5a8939ec49fbf8610c45670017dad8d459694
Shajib <s@ss-MacBook-Pro.lan>
2024-08-15 12:22:10 +0600

    added periodic work request code

--- app/src/main/java/com/shajib/workmanagerandroidpractice/MainActivity.kt
+++ app/src/main/java/com/shajib/workmanagerandroidpractice/MainActivity.kt
@@ -2,15 +2,17 @@ package com.shajib.workmanagerandroidpractice
 
 import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Log
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.work.Data
+import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.ExistingWorkPolicy
 import androidx.work.OneTimeWorkRequest
+import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkInfo
 import androidx.work.WorkManager
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,9 +24,10 @@ class MainActivity : AppCompatActivity() {
             .build()
 
         val workManager = WorkManager.getInstance(this)
+        //one time work request
         val oneTimeWorkRequest = OneTimeWorkRequest.Builder(DownloadWorker::class.java)
             .addTag("download")
-            .setInputData(Data.Builder().putString("url", "https://www.gstatic.com/webp/gallery/1.webp").build())
+            .setInputData(data)
             .build()
 
         workManager.enqueueUniqueWork(
@@ -33,17 +36,35 @@ class MainActivity : AppCompatActivity() {
             oneTimeWorkRequest
         )
 
-        //Taking the WorkInfo from DownloadWorker
-        workManager.getWorkInfoByIdLiveData(oneTimeWorkRequest.id).observe(this, object : Observer<WorkInfo> {
-            override fun onChanged(value: WorkInfo) {
-                if(value.state == WorkInfo.State.SUCCEEDED) {
-                    val filePath = value.outputData.getString("filePath") //get filePath from inputData using key "filePath" from DownloadWorker
-                    val bitmap = BitmapFactory.decodeFile(filePath) //get bitmap from filePath
+        //periodic work request
+        /*val periodicWorkRequest = PeriodicWorkRequestBuilder<DownloadWorker>(
+            15,
+            TimeUnit.MINUTES
+        ) //auto repeat every 15 minutes
+            .addTag("periodicDownload")
+            .setInputData(data)
+            .build()
 
-                    //show image using bitmap
-                    findViewById<ImageView>(R.id.ivImage).setImageBitmap(bitmap)
+        workManager.enqueueUniquePeriodicWork(
+            "periodicDownload" + System.currentTimeMillis().,
+            ExistingPeriodicWorkPolicy.KEEP,
+            periodicWorkRequest
+        )*/
+
+
+        //Taking the WorkInfo from DownloadWorker
+        workManager.getWorkInfoByIdLiveData(oneTimeWorkRequest.id)
+            .observe(this, object : Observer<WorkInfo> {
+                override fun onChanged(value: WorkInfo) {
+                    if (value.state == WorkInfo.State.SUCCEEDED) {
+                        val filePath =
+                            value.outputData.getString("filePath") //get filePath from inputData using key "filePath" from DownloadWorker
+                        val bitmap = BitmapFactory.decodeFile(filePath) //get bitmap from filePath
+
+                        //show image using bitmap
+                        findViewById<ImageView>(R.id.ivImage).setImageBitmap(bitmap)
+                    }
                 }
-            }
-        })
+            })
     }
 }
